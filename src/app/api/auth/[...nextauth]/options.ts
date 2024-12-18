5b9b26d0befc86ae4fec0d029e9f509f521a0d55
arshitcc <arshitcc@gmail.com>
2024-12-19 03:07:42 +0530

    feat(auth) : implement NextAuth for authentication

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,77 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectDB } from "@/lib/db";
+import { User } from "@/models/users.model";
+import bcrypt from "bcrypt";
+
+export const AuthOptions: NextAuthOptions ={
+    providers: [
+        CredentialsProvider({
+            id : "credentials",
+            name : "Credentials",   
+            credentials : {
+                user : {
+                    label : "Username or Email",
+                    type : "text",
+                    placeholder : "Enter username or email"
+                },
+                password : {
+                    label : "Password",
+                    type : "password",
+                    placeholder : "Enter password"
+                }
+            },
+            async authorize(credentials : any) : Promise<any>{
+                await connectDB();
+                try {
+                    console.log(credentials);
+                    const user = await User.findOne({
+                        $or : [{username : credentials.identifier.user}, {email : credentials.identifier.user}]
+                    });
+                    if(!user){
+                        throw new Error("User doesn't exist");
+                    }
+                    if (!user.isVerified) {
+                        throw new Error('Please verify your account before logging in');
+                    }
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password,user.password);
+                    if(isPasswordCorrect){
+                        return user;
+                    }
+                    else{
+                        throw new Error("Incorrect password");
+                    }
+                } catch (error : any) {
+                    throw new Error(error.message);
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if(user){
+                token._id = user._id;
+                token.isVerified = user.isVerified;
+                token.isAcceptingMessage = user.isAcceptingMessages;
+                token.username = user.username;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if(token){
+                session.user._id = token._id;
+                session.user.isVerified = token.isVerified;
+                session.user.isAcceptingMessage = token.isAcceptingMessage;
+                session.user.username = token.username;
+            }
+            return session;
+        }
+    },
+    pages: {
+        signIn: "/login",
+    },
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET
+}
---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+
+export const middleware = async (request: NextRequest) => {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+  if (
+    token &&
+    (url.pathname.startsWith("/login") ||
+      url.pathname.startsWith("/signup") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname.startsWith("/"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/login", "/signup", "/", "/verify/:path*"],
+};
